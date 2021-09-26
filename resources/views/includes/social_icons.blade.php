984c05434eabbcd06ff382146b512cdb72e013be
Nelson Xachatryan <xachatryan_nelson@mail.ru>
2021-09-26 22:30:14 +0400

    change facebook icon link

--- resources/views/includes/social_icons.blade.php
+++ resources/views/includes/social_icons.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <a href="https://www.facebook.com/allaboutall.ru" target="_blank">
+    <a href="https://www.facebook.com/allaboutall.media" target="_blank">
         <i class="fab fa-facebook"></i>
     </a>
     <a href="#"><i class="fab fa-instagram"></i></a>
