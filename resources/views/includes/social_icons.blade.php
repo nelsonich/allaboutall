41a79d28f52e909e538470d23c43dbb12033ddb9
Nelson Xachatryan <xachatryan_nelson@mail.ru>
2021-09-27 23:26:25 +0400

    add telegram icon and site mail info

--- resources/views/includes/social_icons.blade.php
+++ resources/views/includes/social_icons.blade.php
@@ -2,5 +2,9 @@
     <a href="https://www.facebook.com/allaboutall.media" target="_blank">
         <i class="fab fa-facebook"></i>
     </a>
-    <a href="#"><i class="fab fa-instagram"></i></a>
+    <a href="https://t.me/AllAboutAllMedia" target="_blank">
+        <i class="fab fa-telegram"></i>
+    </a>
+    {{-- <a href="#"><i class="fab fa-instagram"></i></a> --}}
+    <a href="mailto:info@allaboutall.media">info@allaboutall.media</a>
 </div>
